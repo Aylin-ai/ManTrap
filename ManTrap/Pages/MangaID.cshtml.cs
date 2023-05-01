40dd24091c097ff024b1810c90279c5b261cc688
Aylin-ai <ajlinkadan3224@gmail.com>
2023-05-01 17:34:51 +0300

    Добавли главную информацию о выбранной манге на страницу MangaID

--- ManTrap/Pages/MangaID.cshtml.cs
+++ ManTrap/Pages/MangaID.cshtml.cs
@@ -44,14 +44,14 @@ namespace ManTrap.Pages
                 cmd.Connection = conn;
 
                 string sql = "select manga.Id, manga.ImageSource, manga.RussianName, manga.EnglishName, " +
-                    "mangatype.Title, mangastatus.Title, translatestatus.Title " +
-                    "manga.YearOfRelease, manga.Overview " +
+                    "mangatype.Title, mangastatus.Title, translatestatus.Title, " +
+                    "manga.YearOfRelease, manga.Overview, manga.OriginalName " +
                     "from manga inner join mangatype " +
                     "on manga.MangaType_Id = mangatype.Id " +
                     "inner join mangastatus " +
-                    "on manga.MangaStatus_Id = mangastatus.Id" +
+                    "on manga.MangaStatus_Id = mangastatus.Id " +
                     "inner join translatestatus " +
-                    "on manga.TranslateStatus.Id = translatestatus.Id " + 
+                    "on manga.TranslateStatus_Id = translatestatus.Id " + 
                     "where manga.Id = @Id and manga.Checked = 1;";
                 cmd.CommandText = sql;
                 cmd.Parameters.AddWithValue("@Id", Id);
@@ -72,6 +72,7 @@ namespace ManTrap.Pages
                             TranslateStatus = reader.GetString(6),
                             YearOfRelease = reader.GetInt32(7),
                             Overview = reader.GetString(8),
+                            OriginalName = reader.GetString(9),
                         };
                     }
                 }
@@ -79,7 +80,8 @@ namespace ManTrap.Pages
 
                 sql = "select mangagenres.Manga_Id, genre.Title " +
                     "from mangagenres inner join genre " +
-                    "on mangagenres.Genre_Id = genre.Id;";
+                    "on mangagenres.Genre_Id = genre.Id " +
+                    "where Manga_Id = @id";
                 cmd.CommandText = sql;
 
                 reader = await cmd.ExecuteReaderAsync();
@@ -87,12 +89,86 @@ namespace ManTrap.Pages
                 {
                     while (reader.Read())
                     {
-                        if (Manga.Id == reader.GetInt32(0))
-                        {
-                            Manga.Genres.Add(reader.GetString(1));
-                        }
+                        Manga.Genres.Add(reader.GetString(1));
                     }
                 }
+                reader.Close();
+
+                sql = "select mangaauthors.Manga_Id, author.Title " +
+                    "from mangaauthors inner join author " +
+                    "on mangaauthors.Author_Id = author.Id " +
+                    "where Manga_Id = @id";
+                cmd.CommandText = sql;
+                reader = await cmd.ExecuteReaderAsync();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Manga.Authors.Add(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+
+                sql = "select mangaartists.Manga_Id, artist.Title " +
+                    "from mangaartists inner join artist " +
+                    "on mangaartists.Artist_Id = artist.Id " +
+                    "where Manga_Id = @id";
+                cmd.CommandText = sql;
+                reader = await cmd.ExecuteReaderAsync();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Manga.Artsits.Add(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+
+                sql = "select mangapublishers.Manga_Id, publisher.Title " +
+                    "from mangapublishers inner join publisher " +
+                    "on mangapublishers.Publisher_Id = publisher.Id " +
+                    "where Manga_Id = @id";
+                cmd.CommandText = sql;
+                reader = await cmd.ExecuteReaderAsync();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Manga.Publishers.Add(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+
+                sql = "select mangareleaseformats.Manga_Id, releaseformat.Title " +
+                    "from mangareleaseformats inner join releaseformat " +
+                    "on mangareleaseformats.ReleaseFormat_Id = releaseformat.Id " +
+                    "where Manga_Id = @id";
+                cmd.CommandText = sql;
+                reader = await cmd.ExecuteReaderAsync();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Manga.ReleaseFormats.Add(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+
+                sql = "select mangatranslateteams.Manga_Id, translateteam.Title " +
+                    "from mangatranslateteams inner join translateteam " +
+                    "on mangatranslateteams.TranslateTeam_Id = translateteam.Id " +
+                    "where Manga_Id = @id";
+                cmd.CommandText = sql;
+                reader = await cmd.ExecuteReaderAsync();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Manga.ReleaseFormats.Add(reader.GetString(1));
+                    }
+                }
+                reader.Close();
+
             }
             catch (Exception ex)
             {
