2a49e65f9a390d3df23cb135a6e3c85ccaef0367
Aylin-ai <ajlinkadan3224@gmail.com>
2023-05-02 19:41:57 +0300

    Реализовал просмотр содержимого заказа

---
+++ ManTrap/Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ManTrap.Models
+{
+    public class OrderItem
+    {
+        public int MangaId { get; set; }
+        public string RussianName { get; set; }
+        public string SourceOfChapterToTranslate { get; set; }
+        public int ChapterId { get; set; }
+    }
+}
--- ManTrap/Pages/OrderComposition.cshtml.cs
+++ ManTrap/Pages/OrderComposition.cshtml.cs
@@ -1,12 +1,80 @@
+using ManTrap.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace ManTrap.Pages
 {
     public class OrderCompositionModel : PageModel
     {
+        [BindProperty(Name = "orderId", SupportsGet = true)]
+        public int OrderId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public OrderCompositionModel()
+        {
+        }
+
         public void OnGet()
         {
+            GetOrderComposition();
+        }
+
+        public IActionResult OnPostMangaIdPage(int id)
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToPage("/MangaId", new { mangaId = id });
+            else
+                return RedirectToPage("Authorization");
+        }
+
+        void GetOrderComposition()
+        {
+            MySqlConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+
+                string sql = "select contractcomposition.Manga_Id, manga.RussianName, " +
+                    "contractcomposition.SourceOfChapterToTranslate, " +
+                    "contractcomposition.ChapterId " +
+                    "from contractcomposition inner join manga " +
+                    "on contractcomposition.Manga_Id = manga.Id " +
+                    "where contractcomposition.Contract_Id = @contractId;";
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@contractId", OrderId);
+
+                List<OrderItem> list = new List<OrderItem>();
+
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new OrderItem()
+                        {
+                            MangaId = reader.GetInt32(0),
+                            RussianName = reader.GetString(1),
+                            SourceOfChapterToTranslate = reader.GetString(2),
+                            ChapterId = reader.GetInt32(3),
+                        });
+                    }
+                }
+                OrderItems = list;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
         }
     }
 }
